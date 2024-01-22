918ecbc55f5acec5e85bbfb37e8ee1f71b540c5f
Lydia Yang <lydia.yang21@imperial.ac.uk>
2024-01-22 12:14:37 +0000

    Dev 62

--- src/User.tsx
+++ src/User.tsx
@@ -1,17 +1,27 @@
-export {};
+export {User};
 
 class User {
   roomCode: string;
-  userId: string;
+  userID: string;
+  displayName: string;
+  isAdmin: boolean;
 
-  constructor(roomCode: string, userId: string) {
+  constructor(
+    roomCode: string,
+    userID: string,
+    displayName: string,
+    isAdmin: boolean
+  ) {
     this.roomCode = roomCode;
-    this.userId = userId;
+    this.userID = userID;
+    this.displayName = displayName;
+    this.isAdmin = isAdmin;
   }
-
 }
 
 // Example usage:
-const user1 = new User("123", "user1");
-console.log(user1.roomCode); // Output: 123
-console.log(user1.userId); // Output: user1
+const user1 = new User("123", "user1", "cyber", true);
+console.log(user1.roomCode);    // Output: 123
+console.log(user1.userID);      // Output: user1
+console.log(user1.displayName)  // Output: cyber
+console.log(user1.isAdmin); // Output: true
