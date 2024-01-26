f071e7aa4f962b190471f2f946939fedf66ec4a2
muxi <yangqinxue333@163.com>
2024-01-26 11:58:10 +0000

    Change macro

--- src/MacroConst.ts
+++ src/MacroConst.ts
@@ -1,9 +1,9 @@
 // Before push, change back to this deployed server
-// export const serverPort = "https://ice.ljthey.co.uk:8080";
-// export const websocketPort = "wss://ice.ljthey.co.uk:8080";
+export const serverPort = "https://ice.ljthey.co.uk:8080";
+export const websocketPort = "wss://ice.ljthey.co.uk:8080";
 
 // When test using localhost, use this local serverPort
-export const serverPort = "http://localhost:8080";
-export const websocketPort = "ws://localhost:8080";
+// export const serverPort = "http://localhost:8080";
+// export const websocketPort = "ws://localhost:8080";
 
 export const refreshTime = 1000;
