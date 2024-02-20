7c3fda39e83da82ac37b73293c682e469688c993
jx1921 <jx1921@ic.ac.uk>
2024-02-20 13:35:56 +0000

    Restore

--- src/macro/MacroServer.tsx
+++ src/macro/MacroServer.tsx
@@ -1,7 +1,7 @@
 // // Before push, change back to this deployed server
-// export const serverPort = "https://ice.ljthey.co.uk:8080";
-// export const websocketPort = "wss://ice.ljthey.co.uk:8080";
+export const serverPort = "https://ice.ljthey.co.uk:8080";
+export const websocketPort = "wss://ice.ljthey.co.uk:8080";
 
 // // When test using localhost, use this local serverPort
-export const serverPort = "http://localhost:8080";
-export const websocketPort = "ws://localhost:8080";
+// export const serverPort = "http://localhost:8080";
+// export const websocketPort = "ws://localhost:8080";
