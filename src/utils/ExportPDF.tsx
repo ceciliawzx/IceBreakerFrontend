c4b75830bfabaccea4c9778dfe89cab1dc30eb78
Lydia Yang <lydia.yang21@imperial.ac.uk>
2024-03-01 14:00:59 +0000

    Resolve ICEBREAKER-256

--- src/utils/ExportPDF.tsx
+++ src/utils/ExportPDF.tsx
@@ -1,34 +1,91 @@
 import jsPDF from "jspdf";
 import { UserProfile } from "../type/UserProfile";
 import exp from "constants";
+import backgroundImage from "../assets/PDFBackground.png";
 
-const exportUserProfileAsPDF = (userProfile: UserProfile) => {
+export const exportUserProfileAsPDF = (userProfile: UserProfile) => {
   console.log("exporting user profile as PDF: ", userProfile.displayName);
   const doc = new jsPDF();
 
-  const lineHeight = 10;
-  let linePosition = 20;
-  if (userProfile.profileImage) {
-    doc.addImage(userProfile.profileImage, "JPEG", 10, linePosition, 50, 50); // Adjust dimensions as needed
-    linePosition += 60; // Adjust based on the size of the image
-  }
-  doc.text(`Display Name: ${userProfile.displayName}`, 10, linePosition);
-  linePosition += lineHeight;
-  doc.text(`First Name: ${userProfile.firstName}`, 10, linePosition);
-  linePosition += lineHeight;
-  doc.text(`Last Name: ${userProfile.lastName}`, 10, linePosition);
-  linePosition += lineHeight;
-  doc.text(`City: ${userProfile.city}`, 10, linePosition);
-  linePosition += lineHeight;
-  doc.text(`Country: ${userProfile.country}`, 10, linePosition);
-  linePosition += lineHeight;
-  doc.text(`Feeling: ${userProfile.feeling}`, 10, linePosition);
-  linePosition += lineHeight;
-  doc.text(`Favorite Food: ${userProfile.favFood}`, 10, linePosition);
-  linePosition += lineHeight;
-  doc.text(`Favorite Activity: ${userProfile.favActivity}`, 10, linePosition);
+  addPDFInfo(doc, userProfile);
 
   doc.save(`${userProfile.displayName} export.pdf`);
+};
+
+// Function to calculate the centered x-coordinate
+const getCenteredX = (doc: any, text: string) => {
+  return (doc.internal.pageSize.width - doc.getTextWidth(text)) / 2;
+};
+
+const printCenteredText = (doc: any, text: string, yPos: number) => {
+  doc.text(text, getCenteredX(doc, text), yPos);
+};
+
+export const addPDFInfo = (doc: any, userProfile: UserProfile) => {
+  // Add background image
+  doc.addImage(
+    backgroundImage,
+    "PNG",
+    0,
+    0,
+    doc.internal.pageSize.width,
+    doc.internal.pageSize.height
+  );
+
+  let linePosition = 50;
+  const textHeight = 15;
+
+  if (userProfile.profileImage) {
+    const imageWidth = 50;
+    const imageHeight = 50;
+    const imageX = 30;
+    const imageY = linePosition;
+    doc.addImage(
+      userProfile.profileImage,
+      "JPEG",
+      imageX,
+      imageY,
+      imageWidth,
+      imageHeight
+    );
+    linePosition += 65; // Adjust based on the size of the image
+  }
+
+  // Name
+  doc.setFontSize(24);
+  doc.setFont("helvetica", "bold");
+
+  printCenteredText(
+    doc,
+    `${userProfile.firstName} ${userProfile.lastName}`,
+    linePosition
+  );
+
+  linePosition += textHeight;
+
+  // Info
+  doc.setFontSize(20);
+  doc.setFont("helvetica", "normal");
+  printCenteredText(
+    doc,
+    `Display Name: ${userProfile.displayName}`,
+    linePosition
+  );
+
+  linePosition += textHeight;
+  printCenteredText(doc, `City: ${userProfile.city}`, linePosition);
+  linePosition += textHeight;
+  printCenteredText(doc, `Country: ${userProfile.country}`, linePosition);
+  linePosition += textHeight;
+  printCenteredText(doc, `Feeling: ${userProfile.feeling}`, linePosition);
+  linePosition += textHeight;
+  printCenteredText(doc, `Favorite Food: ${userProfile.favFood}`, linePosition);
+  linePosition += textHeight;
+  printCenteredText(
+    doc,
+    `Favorite Activity: ${userProfile.favActivity}`,
+    linePosition
+  );
 };
 
 export default exportUserProfileAsPDF;
