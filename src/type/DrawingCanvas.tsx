3619b266da3cb8d72d866b77e773f3ad6c2697d8
Wang, Zixi <zixi.wang21@imperial.ac.uk>
2024-03-02 18:37:59 +0000

    Resolve ICEBREAKER-274

--- src/type/DrawingCanvas.tsx
+++ src/type/DrawingCanvas.tsx
@@ -10,15 +10,28 @@ export interface DrawingData {
 }
 
 export interface DrawingMessage {
-  roomCode: number;
+  roomCode: string;
   drawingData: DrawingData;
   timestamp: string;
   drawer: string;
 }
 
+export interface PasteImgData {
+  imgUrl: string;
+}
+
+export interface PasteImgMessage {
+  roomCode: string;
+  pasteImgData: PasteImgData;
+  timestamp: string;
+  paster: string;
+}
+
 export interface DrawingCanvasProps {
   onDraw: (drawingData: DrawingData) => void;
+  onPaste: (pasteImg: PasteImgData) => void;
   externalDrawing: DrawingMessage | undefined;
+  externalPasteImg: PasteImgMessage | undefined;
   isDrawer: boolean;
   target: any;
 }
